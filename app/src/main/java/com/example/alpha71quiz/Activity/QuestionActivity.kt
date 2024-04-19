ec687d12c9a312c00c72f769bbb6a97e25ee9b67
saimim <saimimislam@gmail.com>
2024-04-19 11:24:44 +0600

    1.32.53

---
+++ app/src/main/java/com/example/alpha71quiz/Activity/QuestionActivity.kt
@@ -0,0 +1,12 @@
+package com.example.alpha71quiz.Activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.alpha71quiz.R
+
+class QuestionActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_question)
+    }
+}
